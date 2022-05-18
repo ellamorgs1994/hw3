886b9b26eeb4db0b1868efe3db2409f0f06d1ba7
ellamorgs1994 <morgan.smith@kellogg.northwestern.edu>
2022-05-18 21:38:42 +0000

    finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,6 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 places = Place.create([{ name: "Mexico City"}, { name: "Charleston"}, { name: "Amsterdam"}])
-Post.create(title: "Surfing!", description: "Took a surfing lesson.", posted_on: "2022-03-22", place_id: 1)
+Post.create(title: "Eating!", description: "All of the food was cheap and yummy", posted_on: "2022-03-22", place_id: 1)
+Post.create(title: "Party boat", description: "Our bachelorette party took a party boat for 2 hours", posted_on: "2022-05-22", place_id: 2)
+Post.create(title: "Coffee shops", description: "A very relaxing tour to find the best greenery in town", posted_on: "2021-01-22", place_id: 3)
