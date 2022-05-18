f837b6cc82811c73861995327cb758cdda08b021
ellamorgs1994 <morgan.smith@kellogg.northwestern.edu>
2022-05-18 20:12:52 +0000

    round 1 done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+places = Place.create([{ name: "Mexico City"}, { name: "Charleston"}, { name: "Amsterdam"}])
+Post.create(title: "Surfing!", description: "Took a surfing lesson.", posted_on: "2022-03-22", place_id: 1)
